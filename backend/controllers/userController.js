05655f4f5aa9f872a9ac3f56a9f763b0dfffa9c3
SpaceSC <ceciliasandor.22@gmail.com>
2021-08-12 20:46:15 +0200

    Reorganize /login endpoint to controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,44 @@
+const fetch = require("node-fetch");
+
+exports.login = async (req, res) => {
+
+  const response = await  fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code: req.body.code,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      redirect_uri: "http://localhost:3000/login",
+      grant_type: "authorization_code"
+    }),
+  })
+
+  const data = await response.json()
+  
+  const decoded = jwt.decode(data.id_token);
+
+  if (!decoded) {
+    return res.status(400).json("Invalid code")
+  }
+
+  console.log(decoded);
+  // destructuring with renaming: sub: google_id
+  const { sub: google_id, given_name, family_name, email, picture } = decoded;
+
+  const filter = { google_id };
+  const update = { google_id, given_name, family_name, picture, email };
+  const user = await User.findOneAndUpdate(filter, update, {
+    setDefaultsOnInsert: true,
+    new: true, // return the new data, but now we don't store it in a variable
+    upsert: true // Make this update into an upsert
+  });
+    
+  const token = jwt.sign({ google_id, picture, given_name, apiStatuses: user.apis }, process.env.JWT_SECRET); // creates jwt signed with mySecret, with payload in {}, user can't use other user access rights
+  
+  // token is a string, now make it into an object {token: token}
+  res.json({ token }); //between {}, token it becomes a key: value pair: token: token
+  
+}
--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,53 +1,12 @@
 const router = require("express").Router();
-const fetch = require("node-fetch");
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const testController = require("../controllers/testController");
+const userController = require("../controllers/userController");
 
 router.get("/test", testController.test);
 
-router.post("/login", async (req, res) => {
-
-  const response = await  fetch("https://oauth2.googleapis.com/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      code: req.body.code,
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      redirect_uri: "http://localhost:3000/login",
-      grant_type: "authorization_code"
-    }),
-  })
-
-  const data = await response.json()
-  
-  const decoded = jwt.decode(data.id_token);
-
-  if (!decoded) {
-    return res.status(400).json("Invalid code")
-  }
-
-  console.log(decoded);
-  // destructuring with renaming: sub: google_id
-  const { sub: google_id, given_name, family_name, email, picture } = decoded;
-
-  const filter = { google_id };
-  const update = { google_id, given_name, family_name, picture, email };
-  const user = await User.findOneAndUpdate(filter, update, {
-    setDefaultsOnInsert: true,
-    new: true, // return the new data, but now we don't store it in a variable
-    upsert: true // Make this update into an upsert
-  });
-    
-  const token = jwt.sign({ google_id, picture, given_name, apiStatuses: user.apis }, process.env.JWT_SECRET); // creates jwt signed with mySecret, with payload in {}, user can't use other user access rights
-  
-  // token is a string, now make it into an object {token: token}
-  res.json({ token }); //between {}, token it becomes a key: value pair: token: token
-  
-})
+router.post("/login", userController.login)
 
 router.post("/toggle-status", async (req, res) => {
 
