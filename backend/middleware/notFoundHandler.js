21824c4d4a50ab9ffddc6e129d4a68f71fd86bc8
SpaceSC <ceciliasandor.22@gmail.com>
2021-08-17 20:52:48 +0200

    Log urls in notFoundHandler

--- backend/middleware/notFoundHandler.js
+++ backend/middleware/notFoundHandler.js
@@ -1,6 +1,6 @@
 // export one function without name, no need to call it, only to require
 module.exports = (req, res, next) => {
-  const error = new Error("Page not found");
+  const error = new Error(`Page not found: ${req.originalUrl}`);
   error.status = 404;
   next(error);
 };
