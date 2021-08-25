32d4c87ca8c43879316d6d0963e51e1642a53f79
SpaceSC <ceciliasandor.22@gmail.com>
2021-08-25 03:51:58 +0200

    Tweak Mission component

--- frontend/src/components/Mission.jsx
+++ frontend/src/components/Mission.jsx
@@ -3,15 +3,14 @@ function Mission({ user }) {
   console.log(user.name);
 
   return (
-    <div>
+    <div className="missionContainer">
       <h2>Mission</h2>
       {user && (
-        <div>
+        <div className="mission">
           <p>{user.given_name}</p>
           <p>
-            `Dear {user.username ? user.username : user.given_name},
-
-            Our mission is to let you create your personal space, where you can see space stuff that you're interested in.`
+            Dear {user.username ? user.username : user.given_name},<br/>
+            Our mission is to let you create your personal space, where you can access space stuff that you're interested in.
           </p>
         </div>
       )}
